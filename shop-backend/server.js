8b446b680cf4b2c1e85820e75ac03b483f109cb1
Michelle Zou <michellezou@Michelles-Air.lan>
2025-09-04 02:16:54 -0400

    add api key to dotenv file and initialize the sdk

--- shop-backend/server.js
+++ shop-backend/server.js
@@ -1,1 +1,15 @@
 // this is where stigg integration will go 
+const express = require('express');
+const cors = require('cors');
+import { Stigg } from '@stigg/node-server-sdk';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const stiggClient = Stigg.initialize({ apiKey: process.env.STIGG_API_KEY });
+if (!process.env.STIGG_API_KEY){
+    console.error('make sure to add your Stigg API key to the dotenv file')
+}
+
+export default stiggClient;
